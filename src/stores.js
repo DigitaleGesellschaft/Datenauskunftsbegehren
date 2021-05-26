ddaa8e01959039498c760d15cb94dd9de73e2a81
Beni Buess <beni@benel.net>
2021-05-26 15:22:17 +0200

    fix: throttle history update to not run into a security error because of more than 100 replaceState calls per 30s, fixes #12

--- src/stores.js
+++ src/stores.js
@@ -1,24 +1,8 @@
 import { readable, writable, get } from 'svelte/store';
 import {nl2br, br2nl, ab2str} from './lib.js'
+import { throttle } from 'lodash-es';
 import d from './data.js';
 export const data = readable(d)
-
-let lastStep
-
-const currentUserData = getUserDataFromHash()
-export const userData = writable(currentUserData)
-userData.subscribe(val => {
-  // only if the step changes, we want to push a new state to the history
-  // otherwise we are going to replace the state
-  let pushState = false
-  if (val.step) {
-    if (val.step !== lastStep) {
-      lastStep = val.step;
-      pushState = true;
-    }
-  }
-  setUserDataToHash(val, pushState)
-})
 
 window.addEventListener('hashchange', (event) => {
   userData.update(() => {
@@ -38,18 +22,43 @@ function getUserDataFromHash() {
   }
 }
 
-function setUserDataToHash(data, pushState = false) {
+let lastStep
+function setUserDataToHash(data) {
   // we version everything so we can break the data model in the future
   // but still support v1
   data.v = 1
   const hash = encodeURI(JSON.stringify(data))
   const title = `Datenauskunftsbegehren - ${data.step}`;
+
+  // only if the step changes, we want to push a new state to the history
+  // otherwise we are going to replace the state
+  let pushState = false
+  if (data.step) {
+    if (data.step !== lastStep) {
+      lastStep = data.step;
+      pushState = true;
+    }
+  }
+
   if (pushState) {
     window.history.pushState({step: data.step}, title, `#${hash}`)
   } else {
     window.history.replaceState({step: data.step}, title, `#${hash}`)
   }
 }
+
+// Safari throws if replaceState is called more than 100 times in 30s
+// we throttle to update max once per 300ms
+const setUserDataToHashThrottled = throttle(setUserDataToHash, 300, {
+  leading: true,
+  trailing: true
+})
+
+const currentUserData = getUserDataFromHash()
+export const userData = writable(currentUserData)
+userData.subscribe(val => {
+  setUserDataToHashThrottled(val)
+})
 
 export const userAddressHtml = writable(nl2br(currentUserData.address))
 userAddressHtml.subscribe(address => {
