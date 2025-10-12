05cc601db7b2bea4887bcf8c7dfdf148dd6b4a56
nielsson4711 <nilsdev21@pm.me>
2025-10-12 19:03:32 +0200

    moved text strings from index.html template into vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,12 @@ export default defineConfig({
       template: 'index.html',
       inject: {
         data: {
+          // @TODO Redaktion description_short: Shorten description for SEO purposes or leave the long version?
+          browser_lang: 'de',
+          page_title: 'Generiere dein Datenauskunftsbegehren | Digitale Gesellschaft',
+          description: 'Gemäss Datenschutzgesetz hat jede Person das Recht zu erfahren, welche Daten über sie gespeichert sind, und diese – wenn nötig – löschen oder korrigieren zu lassen.',
+          description_short: 'Gemäss Datenschutzgesetz hat jede Person das Recht zu erfahren, welche Daten über sie gespeichert sind',
+          browser_outdated: '<p>Du verwendest einen alten Browser. <br>Der Generator funktioniert damit leider nicht.</p><p>Bitte verwende eine aktuelle Version des Browsers deiner Wahl.</p>'
         },
       },
     })
