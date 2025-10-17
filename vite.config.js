e94b0b757ad96b4d1b039d28b4ecb8b6c00dfa5c
PonyM <dev@lksch.ch>
2025-10-17 19:10:01 +0200

    Migrate texts to html and remove plugin
    - plugins last release is 2023

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,9 @@
-import { defineConfig } from 'vite';
-import { svelte } from '@sveltejs/vite-plugin-svelte';
-import { createHtmlPlugin } from 'vite-plugin-html';
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
 
-
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    // commonjs(),
-    svelte(),
-    createHtmlPlugin({
-      minify: true,
-      entry: 'src/main.js',
-      template: 'index.html',
-      inject: {
-        data: {
-          // @TODO Redaktion description_short: Shorten description for SEO purposes or leave the long version?
-          browser_lang: 'de',
-          page_title: 'Generiere dein Datenauskunftsbegehren | Digitale Gesellschaft',
-          description: 'Gemäss Datenschutzgesetz hat jede Person das Recht zu erfahren, welche Daten über sie gespeichert sind, und diese – wenn nötig – löschen oder korrigieren zu lassen.',
-          description_short: 'Gemäss Datenschutzgesetz hat jede Person das Recht zu erfahren, welche Daten über sie gespeichert sind',
-          browser_outdated: '<p>Du verwendest einen alten Browser. <br>Der Generator funktioniert damit leider nicht.</p><p>Bitte verwende eine aktuelle Version des Browsers deiner Wahl.</p>'
-        },
-      },
-    })
-  ],
-
+  plugins: [svelte()],
   server: {
     open: true
   },
@@ -34,21 +14,4 @@ export default defineConfig({
   optimizeDeps: {
     include: ['lodash-es'],
   },
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-    rollupOptions: {
-      input: 'src/main.js',
-      output: {
-        entryFileNames: 'build/[name]-[hash].js',
-        chunkFileNames: 'build/[name]-[hash].js',
-        assetFileNames: 'build/[name]-[hash].[ext]',
-      },
-      sourcemap: true,
-      name: 'app',
-    },
-  },
-  base: './',
-});
-
-
+})
