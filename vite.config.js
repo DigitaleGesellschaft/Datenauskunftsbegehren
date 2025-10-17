63cbfd7633d8d7cb69766766a72a25e39509529c
PonyM <dev@lksch.ch>
2025-10-17 19:10:01 +0200

    Migrate texts to html and remove plugin
    - plugins last release is 2023

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,9 @@
-import { defineConfig } from 'vite';
-import { svelte } from '@sveltejs/vite-plugin-svelte';
-import fs from 'fs';
-import path from 'path';
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
 
-const htmlVars = {
-  browser_lang: 'de',
-  page_title: 'Generiere dein Datenauskunftsbegehren | Digitale Gesellschaft',
-  description: 'Gemäss Datenschutzgesetz hat jede Person das Recht zu erfahren, welche Daten über sie gespeichert sind, und diese – wenn nötig – löschen oder korrigieren zu lassen.',
-  description_short: 'Gemäss Datenschutzgesetz hat jede Person das Recht zu erfahren, welche Daten über sie gespeichert sind',
-  browser_outdated: '<p>Du verwendest einen alten Browser.<br>Der Generator funktioniert damit leider nicht.</p><p>Bitte verwende eine aktuelle Version des Browsers deiner Wahl.</p>'
-};
-
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    svelte(),
-    {
-      name: 'html-template-vars',
-      transformIndexHtml(html) {
-        // einfache Platzhalter-Ersetzung: %varname%
-        return html.replace(/%(\w+)%/g, (_, key) => htmlVars[key] ?? '');
-      }
-    }
-  ],
+  plugins: [svelte()],
   server: {
     open: true
   },
@@ -29,11 +11,7 @@ export default defineConfig({
     open: true,
     port: 8080,
   },
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-    sourcemap: true
+  optimizeDeps: {
+    include: ['lodash-es'],
   },
-  base: './',
-});
-
+})
