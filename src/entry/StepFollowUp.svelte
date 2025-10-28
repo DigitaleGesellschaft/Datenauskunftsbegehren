356ccdaafc337e4e9803b803f848b538561f8801
PonyM <dev@lksch.ch>
2025-10-28 20:23:25 +0100

    add translations step follow up

--- src/entry/StepFollowUp.svelte
+++ src/entry/StepFollowUp.svelte
@@ -1,5 +1,6 @@
 <script>
   import { run } from 'svelte/legacy';
+  import { _ } from 'svelte-i18n';
 
   import { data, userData } from '../stores.js';
   import UserAddress from './UserAddress.svelte';
@@ -15,19 +16,19 @@
 </script>
 
 {#if selectedDesire}
-  <h2>Mach noch einige Angaben für das Auskunftsbegehren<br> «{selectedDesire.label}»</h2>
-  <div class="data-entry-form">
-    <section>
-      <UserAddress></UserAddress>
-    </section>
-    <section>
-      <h3>Organisation</h3>
-      <label for="org">Wähle eine Organisation</label>
-      <OrgSelection bind:org={$userData.org}></OrgSelection>
-      <label for="orgAddress">oder gib die Adresse ein</label>
-      <textarea id="orgAddress" bind:value={$userData.orgAddressEntry} rows="4"></textarea>
-    </section>
-  </div>
+   <h2>{$_("step_followup.instructions", { default: "Mach noch einige Angaben für das Auskunftsbegehren" })}<br> «{selectedDesire.label}»</h2>
+   <div class="data-entry-form">
+     <section>
+       <UserAddress></UserAddress>
+     </section>
+     <section>
+       <h3>{$_("organisation", { default: "Organisation" })}</h3>
+       <label for="org">{$_("select_organisation", { default: "Wähle eine Organisation" })}</label>
+       <OrgSelection bind:org={$userData.org}></OrgSelection>
+       <label for="orgAddress">{$_("or_enter_address", { default: "oder gib die Adresse ein" })}</label>
+       <textarea id="orgAddress" bind:value={$userData.orgAddressEntry} rows="4"></textarea>
+     </section>
+   </div>
 {:else}
-  <h2>Etwas hat nicht funktioniert "userData.step" ist "undef" oder nicht in der Auswahl. userData.desire: {$userData.desire}</h2>
+  <h2>{$_("error.something_went_wrong", { default: "Etwas hat nicht funktioniert: userData.step ist undef oder nicht in der Auswahl. userData.desire: " })} userData.desire: {$userData.desire}</h2>
 {/if}
