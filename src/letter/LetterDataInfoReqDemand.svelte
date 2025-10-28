872e5f160d4f4fc3d3c2d44e7fe261bd04f7527b
PonyM <dev@lksch.ch>
2025-10-28 20:06:01 +0100

    add letter req demand

--- src/letter/LetterDataInfoReqDemand.svelte
+++ src/letter/LetterDataInfoReqDemand.svelte
@@ -2,6 +2,7 @@
   import { run } from 'svelte/legacy';
 
   import {onMount} from 'svelte';
+  import { _ } from 'svelte-i18n';
   import HideNodeAction from './HideNodeAction.svelte';
   import {data, orgAddressHtml, userAddressHtml, userData} from '../stores.js';
   import {nl2br} from '../lib.js';
@@ -67,97 +68,97 @@
   <section id="letter" bind:this={LetterDataInfoReqDemandNode}>
     <div class="letter-head">
       <div class="address-from">
-        <span
-          contenteditable
-          spellcheck="false"
-          class="editable-variable"
-          data-label="Dein Name"
-          class:empty={!$userData.name || $userData.name.length === 0}
-          bind:innerHTML={$userData.name}>
-        </span>
+         <span
+           contenteditable
+           spellcheck="false"
+           class="editable-variable"
+           data-label={$_("your_name")}
+           class:empty={!$userData.name || $userData.name.length === 0}
+           bind:innerHTML={$userData.name}>
+         </span>
         <br>
-        <span
-          contenteditable
-          spellcheck="false"
-          class="editable-variable"
-          data-label="Deine Adresse"
-          class:empty={!$userAddressHtml || $userAddressHtml.length === 0}
-          bind:innerHTML={$userAddressHtml}></span>
+         <span
+           contenteditable
+           spellcheck="false"
+           class="editable-variable"
+           data-label={$_("your_address")}
+           class:empty={!$userAddressHtml || $userAddressHtml.length === 0}
+           bind:innerHTML={$userAddressHtml}></span>
           
           <!-- chromium has a bug, and needs this empty span -->
           <span>&nbsp;</span>
       </div>
 
       <div class="address-to">
-        <div contenteditable spellcheck="false">
-          EINSCHREIBEN
-        <HideNodeAction title="Ein-/ausblenden"></HideNodeAction>
-        </div>
+       <div contenteditable spellcheck="false">
+           {$_("registered_mail")}
+         <HideNodeAction title={$_("toggle_visibility")}></HideNodeAction>
+         </div>
         <br>
         {#if orgAddressTo.length > 0}
           {@html orgAddressTo}
         {:else}
-          <span
-            contenteditable
-            spellcheck="false"
-            class="editable-variable"
-            data-label="Empfängeradresse"
-            class:empty={!$orgAddressHtml || $orgAddressHtml.length === 0}
-            bind:innerHTML={$orgAddressHtml}>
-          </span>
+           <span
+             contenteditable
+             spellcheck="false"
+             class="editable-variable"
+             data-label={$_("recipient_address")}
+             class:empty={!$orgAddressHtml || $orgAddressHtml.length === 0}
+             bind:innerHTML={$orgAddressHtml}>
+           </span>
         {/if}
         <br><br><br>
       </div>
-      <p
-        class="date editable-variable"
-        contenteditable
-        spellcheck="false"
-        data-label="Datum"
-        class:empty={!$userData.date || $userData.date.length === 0}
-        bind:innerHTML={$userData.date}>
-      </p>
+       <p
+         class="date editable-variable"
+         contenteditable
+         spellcheck="false"
+         data-label={$_("date")}
+         class:empty={!$userData.date || $userData.date.length === 0}
+         bind:innerHTML={$userData.date}>
+       </p>
     </div>
 
     <h1 class="subject" contenteditable spellcheck="false">
-      Datenauskunftsbegehren
+      {$_("data_info_request")}
     </h1>
 
     <p class="salutation" contenteditable spellcheck="false">
-      Sehr geehrte Angesprochene
+      {$_("salutation")}
     </p>
 
     <p contenteditable spellcheck="false">
-      Mein Datenauskunftsbegehren vom TT. MMMM JJJJ wurde mit Ihrer Auskunft vom TT. MMMM JJJJ nicht vollständig beantwortet.
+      {$_("letter_info_req_demand_incomplete", {default: "Mein Datenauskunftsbegehren vom TT. MMMM JJJJ wurde mit Ihrer Auskunft vom TT. MMMM JJJJ nicht vollständig beantwortet."})}
     </p>
     <p contenteditable spellcheck="false">
-      Ich gehe davon aus, dass insbesondere folgende Informationen fehlen:
+      {$_("letter_info_req_demand_missing_info", {default: "Ich gehe davon aus, dass insbesondere folgende Informationen fehlen:"})}
     </p>
-    <ul>
-      <li contenteditable spellcheck="false">[Auflistung der fehlenden Daten]</li>
-      <li contenteditable spellcheck="false">...</li>
-    </ul>
+     <ul>
+       <li contenteditable spellcheck="false">{$_("letter_info_req_demand_list_missing", {default: "[Auflistung der fehlenden Daten]"})}</li>
+       <li contenteditable spellcheck="false">...</li>
+     </ul>
     <p contenteditable spellcheck="false">
-      Gemäss Art. 18 der Verordnung über den Datenschutz (Datenschutzverordnung, DSV) vom 31. August 2022 müssen Angaben geliefert werden, wieso eine Auskunft eingeschränkt wurde. Ich bitte Sie entsprechend, vollständig Auskunft zu erteilen oder Angaben zu liefern, wieso die Auskunft eingeschränkt wurde..
+      {$_("letter_info_req_demand_refusal", {default: "Gemäss Art. 18 der Verordnung über den Datenschutz (Datenschutzverordnung, DSV) vom 31. August 2022 müssen Angaben geliefert werden, wieso eine Auskunft eingeschränkt wurde. Ich bitte Sie entsprechend, vollständig Auskunft zu erteilen oder Angaben zu liefern, wieso die Auskunft eingeschränkt wurde.."})}
     </p>
     <p contenteditable spellcheck="false">
-      Sollten Sie weiterhin keine vollständige Auskunft erteilen, behalte ich mir jederzeit vor, mein Auskunftsrecht auf dem Rechtsweg durchzusetzen oder Anzeige beim Eidgenössischen Datenschutz- und Öffentlichkeitsbeauftragten (EDÖB) zu erstatten. Ich weise ausserdem darauf hin, dass das vorsätzliche Erteilen einer falschen oder unvollständigen Auskunft auf Antrag mit Busse bis zu 250'000 Franken bestraft werden kann (Art. 60 DSG).
+      {$_("letter_info_req_demand_enforcement", {default: "Sollten Sie weiterhin keine vollständige Auskunft erteilen, behalte ich mir jederzeit vor, mein Auskunftsrecht auf dem Rechtsweg durchzusetzen oder Anzeige beim Eidgenössischen Datenschutz- und Öffentlichkeitsbeauftragten (EDÖB) zu erstatten. Ich weise ausserdem darauf hin, dass das vorsätzliche Erteilen einer falschen oder unvollständigen Auskunft auf Antrag mit Busse bis zu 250'000 Franken bestraft werden kann (Art. 60 DSG)."})}
     </p>
 
-    <div class="no-break-inside">
-      <p contenteditable spellcheck="false" class="no-break-after">
-        Besten Dank und freundliche Grüsse
-      </p>
-      <br><br>
-      <p
-        contenteditable
-        spellcheck="false"
-        class="editable-variable"
-        data-label="Dein Name"
-        class:empty={!$userData.name || $userData.name.length === 0}
-        bind:innerHTML={$userData.name}>
-      </p>
+     <div class="no-break-inside">
+       <p contenteditable spellcheck="false" class="no-break-after">
+         {$_("closing")}
+       </p>
+       <br><br>
+       <p
+         contenteditable
+         spellcheck="false"
+         class="editable-variable"
+         data-label={$_("your_name")}
+         class:empty={!$userData.name || $userData.name.length === 0}
+         bind:innerHTML={$userData.name}>
+       </p>
 
-    </div>
+     </div>
   </section>
 </div>
 
