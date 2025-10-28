c155db53092e341e2dbd1fe2e45c99632851dea3
PonyM <dev@lksch.ch>
2025-10-28 20:30:12 +0100

    add markers step Type, now new strings

--- src/entry/StepType.svelte
+++ src/entry/StepType.svelte
@@ -3,6 +3,7 @@
   import VariableInput from '../VariableInput.svelte';
   import UserAddress from './UserAddress.svelte';
   import OrgSelection from './OrgSelection.svelte';
+  import { _ } from 'svelte-i18n';
 
   let selectedType = $derived($data.getType($userData.types[0]))
   let variables = $derived(selectedType ? selectedType.variables : [])
@@ -11,11 +12,11 @@
 </script>
 
 {#if selectedType}
-  <h2>Mach noch einige Angaben für das Auskunftsbegehren «{selectedType.label}»</h2>
+  <h2>{$_("provide_more_details", { values: { orgName: selectedType.label } })}</h2>
   <div class="data-entry-form">
     {#if variables.length > 0}
       <section>
-        <h3>Deine Angaben</h3>
+        <h3>{$_("your_details")}</h3>
         {#each variables as variable}
           <VariableInput {variable} bind:val="{$userData[variable.name]}"></VariableInput>
         {/each}
@@ -25,10 +26,10 @@
       <UserAddress></UserAddress>
     </section>
     <section>
-      <h3>Organisation</h3>
-      <label for="org">Wähle eine Organisation</label>
+      <h3>{$_("organisation")}</h3>
+      <label for="org">{$_("select_organisation")}</label>
       <OrgSelection bind:org={$userData.org} bind:options={orgsWithType}></OrgSelection>
-      <label for="orgAddress">oder gib die Adresse ein</label>
+      <label for="orgAddress">{$_("or_enter_address")}</label>
       <textarea id="orgAddress" bind:value={$userData.orgAddressEntry} rows="4"></textarea>
     </section>
   </div>
